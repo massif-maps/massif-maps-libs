cab6c5a23b03b251e6160273480217c5dc0ec276
Martin Guillon <dev@akylas.fr>
2022-11-09 15:20:31 +0100

    chore: testing revert hillshade chnages

--- vt/src/vt/NormalMapBuilder.cpp
+++ vt/src/vt/NormalMapBuilder.cpp
@@ -58,54 +58,55 @@ namespace carto::vt {
             std::vector<cglib::vec3<float>> buffer(subWidth * subHeight, cglib::vec3<float>(0.0f, 0.0f, 0.0f));
 
             float heights[3][3];
-            for (int y = 0; y < subHeight; y++) {
-                double y1 = boost::math::constants::pi<double>() * ((tileId.y + (height - y0 - y - 0.5) / height) / (1 << tileId.zoom) - 0.5);
+            for (int y = 0; y < height; y++) {
+                double y1 = boost::math::constants::pi<double>() * ((tileId.y + (height - y - 0.5) / height) / (1 << tileId.zoom) - 0.5);
                 double rz = std::tanh(y1);
                 double ss = std::sqrt(std::max(0.0, 1.0 - rz * rz));
 
                 for (int dy = 0; dy < 3; dy++) {
-                    heights[dy][1] = getInterpolatedHeight(x0 - 1, y0 + y + dy - 1);
-                    heights[dy][2] = getInterpolatedHeight(x0 - 0, y0 + y + dy - 1);
+                    heights[dy][1] = getInterpolatedHeight( - 1,   y + dy - 1);
+                    heights[dy][2] = getInterpolatedHeight( - 0,   y + dy - 1);
                 }
-                for (int x = 0; x < subWidth; x++) {
+                for (int x = 0; x < width; x++) {
                     for (int dy = 0; dy < 3; dy++) {
                         heights[dy][0] = heights[dy][1];
                         heights[dy][1] = heights[dy][2];
-                        heights[dy][2] = getInterpolatedHeight(x0 + x + 1, y0 + y + dy - 1);
+                        heights[dy][2] = getInterpolatedHeight( x + 1, y + dy - 1);
                     }
 
                     float dx = (heights[0][2] + 2 * heights[1][2] + heights[2][2]) - (heights[0][0] + 2 * heights[1][0] + heights[2][0]);
                     float dy = (heights[2][0] + 2 * heights[2][1] + heights[2][2]) - (heights[0][0] + 2 * heights[0][1] + heights[0][2]);
                     float dz = 8.0f * static_cast<float>(ss);
 
-                    buffer[y * subWidth + x] = cglib::vec3<float>(dx, dy, dz);
+                    data[y * width + x] = packNormal(cglib::vec3<float>(dx, dy, dz));
+                    // buffer[y * subWidth + x] = cglib::vec3<float>(dx, dy, dz);
                 }
             }
 
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    cglib::vec3<float> normal(0.0f, 0.0f, 0.0f);
-                    if (delta == 0) {
-                        normal = buffer[y * subWidth + x];
-                    } else {
-                        int sx = x >> delta;
-                        int sy = y >> delta;
-                        float wx = (x - (sx << delta) + 0.5f) / (1 << delta);
-                        float wy = (y - (sy << delta) + 0.5f) / (1 << delta);
-                        for (int dy = 0; dy < 2; dy++) {
-                            wy = 1.0f - wy;
-                            for (int dx = 0; dx < 2; dx++) {
-                                wx = 1.0f - wx;
-                                if (sx + dx < subWidth && sy + dy < subHeight) {
-                                    normal += buffer[(sy + dy) * subWidth + sx + dx] * wx * wy;
-                                }
-                            }
-                        }
-                    }
+            // for (int y = 0; y < height; y++) {
+            //     for (int x = 0; x < width; x++) {
+            //         cglib::vec3<float> normal(0.0f, 0.0f, 0.0f);
+            //         if (delta == 0) {
+            //             normal = buffer[y * subWidth + x];
+            //         } else {
+            //             int sx = x >> delta;
+            //             int sy = y >> delta;
+            //             float wx = (x - (sx << delta) + 0.5f) / (1 << delta);
+            //             float wy = (y - (sy << delta) + 0.5f) / (1 << delta);
+            //             for (int dy = 0; dy < 2; dy++) {
+            //                 wy = 1.0f - wy;
+            //                 for (int dx = 0; dx < 2; dx++) {
+            //                     wx = 1.0f - wx;
+            //                     if (sx + dx < subWidth && sy + dy < subHeight) {
+            //                         normal += buffer[(sy + dy) * subWidth + sx + dx] * wx * wy;
+            //                     }
+            //                 }
+            //             }
+            //         }
 
-                    data[y * width + x] = packNormal(normal);
-                }
-            }
+            //         data[y * width + x] = packNormal(normal);
+            //     }
+            // }
         }
         return std::make_shared<Bitmap>(width, height, std::move(data));
     }
